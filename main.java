a70bd8d8510a1df33ba1728bc4991520d5c857e3
Louis Pelletier <louispelletier@Louiss-MacBook-Pro.local>
2024-09-09 19:19:10 -0400

    Étape 1

--- main.java
+++ main.java
@@ -1,3 +1,10 @@
 public class main {
+    public static void main(String[] args) {
+        // Ceci n'est pas un test!! C'est un exemple d'utilisation.
+        TriageType doctorTriageType = TriageType.FIFO;
+        TriageType radiologyTriageType = TriageType.FIFO;
 
+        // Clinic clinic = new Clinic(doctorTriageType, radiologyTriageType /* , ... */)
+        // clinic.triagePatient("John", 4, VisibleSymptom.MIGRAINE);
+    }
 }
